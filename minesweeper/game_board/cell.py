5a3d6fe3479edb5e689e2f8a76b87cbcb6ef25f2
Phylumium0001 <phylumium3000@gmail.com>
2024-10-21 09:22:27 +0100

    optimized add_mines method

--- minesweeper/game_board/cell.py
+++ minesweeper/game_board/cell.py
@@ -7,7 +7,25 @@ class Cell:
         self.is_revealed = False
         self.is_mine = is_mine
 
+    def add_mine(self):
+        self.is_mine = True
+    
+    def __str__(self) -> str:
+        return f"Cell(Revealed : {self.is_revealed}, Is_mine : {self.is_mine})"
+    
+    def __repr__(self) -> str:
+        if self.is_revealed:
+            s = "R"
+        else:
+            s = "H"
+
+        if self.is_mine:
+            v = "M"
+        else:
+            v = "S"
+        return f"({s},{v})"
+    
 if __name__ == "__main__":
     cell1 = Cell()
     cell2 = Cell(is_mine=True)
-    
+
--- minesweeper/game_board/game_board.py
+++ minesweeper/game_board/game_board.py
@@ -1,4 +1,43 @@
+from cell import Cell
+import random
+ 
 class Board:
     def __init__(self,dimension) -> None:
         self.dimension = dimension
-         
+        self.x,self.y = self.dimension
+        # Generate the board using the dimensions
+        self.board = []
+        self.generate_board()
+        
+    def generate_board(self):
+        # Use the dimensions to determine the size of board
+        self.populate_board()
+        self.add_mines()
+
+    def populate_board(self):
+        for i in range(self.x):
+            inner_board = []
+
+            for j in range(self.y):
+                # Add cell to inner board
+                inner_board.append(Cell())
+            # Add inner board to main board
+            self.board.append(inner_board)
+
+    def add_mines(self):
+        num_of_mines = self.x
+        all_cell_indexes = [(i,j) for i in range(self.x) for j in range(self.y)]
+        random.shuffle(all_cell_indexes)
+
+        for i,j in all_cell_indexes[:num_of_mines]:
+            print(i,j)
+            self.board[i][j].add_mine() 
+    
+    def check_cell_neighbours(self):
+        # Return the number adjacent mine cells
+        pass
+
+if __name__ == "__main__":
+    board = Board((8,8))
+
+    print(board.board)
