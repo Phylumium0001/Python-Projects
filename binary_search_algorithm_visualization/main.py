7e1f6692e7cd46f1fe3363c513655f93864861bd
Phylumium0001 <phylumium3000@gmail.com>
2025-08-17 22:04:12 +0000

    Fixing formatting

--- binary_search_algorithm_visualization/main.py
+++ binary_search_algorithm_visualization/main.py
@@ -1,9 +1,11 @@
 from binary_search import search
 
+
 def main():
-    array = [int(i) for i in input("Enter the sorted array,seperated by commas : ").split(",")]
+    array = [int(i) for i in input(
+        "Enter the sorted array,seperated by commas : ").split(",")]
     number = int(input("Which number needs to be found : "))
-    print(search(array,number))
+    print(search(array, number))
 
 
 if __name__ == "__main__":
