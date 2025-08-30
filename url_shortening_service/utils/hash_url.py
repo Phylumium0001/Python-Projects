4c0ce5e99b1f4a997261b3a6f2359fca6c26b926
Phylumium0001 <phylumium3000@gmail.com>
2025-08-30 23:37:33 +0000

    Com4

--- url_shortening_service/utils/hash_url.py
+++
@@ -1,9 +0,0 @@
-# Hash Url
-from hashlib import sha256
-from os import urandom
-
-
-def generate_hash_value(normalized_url: str):
-    url_byte = bytes(normalized_url, encoding="utf-8")
-    hash = sha256(url_byte)
-    return hash.hexdigest()
