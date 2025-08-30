c6e93ef65ec7e6531c6ecdda25edfabe8cd36214
Phylumium0001 <phylumium3000@gmail.com>
2025-08-30 09:53:21 +0000

    Implemented REST APIs

--- url_shortening_service/backend/server.py
+++ url_shortening_service/backend/server.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
-
+from utils.generate_short_url import generate_url
+from utils.hash_url import generate_hash_value
+from utils.normalize import normalize_url
+from utils.database import Database
 
 app = FastAPI()
 
@@ -17,16 +21,40 @@ class PostResponse(BaseModel):
 
 
 @app.get("/{short_code}")
-def unshorten(url):
+def unshorten(short_code: str):
+    url = "shlk.com/" + short_code
+
+    print(url)
+
     # Match Found
-    # Redirect the user to the page :  RedirectResponse(url)
+    db = Database()
+    res = db.find_url(url)
+    print(f"%%%%%%%%%%%%%%%%%{res}%%%%%%%%%%%%%%%%%%%")
+
+    if res:
+        # Redirect the user to the page :  RedirectResponse(url)
+        short_url, long_url = res
+        return RedirectResponse(url=long_url, status_code=307)
+
+        # return {"long_url": long_url, "short_url": short_url}
 
     # Match Not Found
-    pass
+    else:
+        HTTPException(status_code=404, detail="Page Not Found")
 
 # POST /shorten
 
 
 @app.post("/shorten")
-def shorten(new_url, response_model=PostResponse):
-    pass
+def shorten(long_url: str) -> PostResponse:
+    normalized_url = normalize_url(long_url)
+    hash = generate_hash_value(normalized_url)
+    short_url = generate_url(hash)
+
+    # TODO : Store to database
+
+    # Return the Response
+    if short_url:
+        return {"long_url": long_url, "short_url": short_url}
+    else:
+        HTTPException(status_code=400, detail="Failed to shorten")
